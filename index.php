92a700e69e6092ad49cc25b8d0e24287d45f9881
mystic <matthew.poletiek@gmail.com>
2022-01-13 07:35:30 +0000

    Stats

---
+++ index.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>TechnoMystics.com</title>
+
+    <!-- Bootstrap core CSS -->
+<link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+<link rel="icon" href="/assets/img/favicons/favicon.ico">
+<meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="/css/index.css" rel="stylesheet">
+  </head>
+  <body class="d-flex h-100 text-center text-white bg-dark">
+    
+<div class=" d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0"><img src="/media/pics/technomystic.png" height="100px"></h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+	<a class="nav-link" href="https://social.technomystics.com">Social</a>
+	<a class="nav-link" href="https://discourse.technomystics.com">Discourse</a>
+	<a class="nav-link" href="https://matrix.to/#/#the-lodge:matrix.technomystics.com">Matrix</a>
+	<a class="nav-link" href="https://mail.technomystics.com">Mail</a>
+	<a class="nav-link" href="https://technomystics.com/stats.php">Stats</a>
+	<!--
+	<a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Contact</a>
+	-->
+      </nav>
+    </div>
+  </header>
+
+  <main class="px-3">
+    <!--
+    <h1>TechnoMystics.com</h1>
+    -->
+    <p class="lead"><img src="/media/pics/techno.png" height="100px"> + <img src="/media/pics/mystic.png" height="100px"> = <img src="/media/pics/technomystic.png" height="100px"></p>
+    <p class="lead">
+	<!--
+      <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+	-->
+    </p>
+  </main>
+
+  <footer class="mt-auto text-white-50">
+
+<!--
+    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+-->
+
+  </footer>
+</div>
+
+
+    
+  </body>
+</html>
---
+++ stats.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+	<title>Stats.TechnoMystics.com</title>
+<!-- Bootstrap core CSS -->
+<link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+<link rel="icon" href="/assets/img/favicons/favicon.ico">
+<meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="/css/index.css" rel="stylesheet">
+</head>
+
+
+<body class="d-flex h-100 w-100 text-center text-white bg-dark">
+<div class=" d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0"><img src="/media/pics/technomystic.png" height="100px"></h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link" aria-current="page" href="/">Home</a>
+	<a class="nav-link" href="https://social.technomystics.com">Social</a>
+	<a class="nav-link" href="https://discourse.technomystics.com">Discourse</a>
+	<a class="nav-link" href="https://matrix.to/#/#the-lodge:matrix.technomystics.com">Matrix</a>
+	<a class="nav-link" href="https://mail.technomystics.com">Mail</a>
+	<a class="nav-link active" href="https://stats.technomystics.com">Stats</a>
+	<!--
+	<a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Contact</a>
+	-->
+      </nav>
+    </div>
+  </header>
+<hr>
+
+	<div class="w-100 h-100 p-3">
+	<iframe src="https://stats.technomystics.com:3000/d/lj6brF17k/stats-technomystics-com?orgId=1&kiosk" width="100%" height="100%" frameborder="0"></iframe>
+	</div>
+</div>
+
+</body>
+</html>
